bd0abdc87e8ca76d4db4133ca685245930074720
Muhammad Wassam <muhammadwassam450@gmail.com>
2025-04-25 09:54:21 +0500

    Quiz 3 file

--- main.dart
+++ main.dart
@@ -1,99 +1,123 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(QuizApp());
-}
+void main() => runApp(MyApp());
 
-class QuizApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: MainScreen(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.lightGreen),
-      home: QuizScreen(),
     );
   }
 }
 
-class QuizScreen extends StatefulWidget {
+class MainScreen extends StatefulWidget {
   @override
-  _QuizScreenState createState() => _QuizScreenState();
+  _MainScreenState createState() => _MainScreenState();
 }
 
-class _QuizScreenState extends State<QuizScreen> {
-  int _questionIndex = 0;
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
 
-  final List<Map<String, Object>> _questions = [
-    {'question': 'Flutter is developed by Facebook.', 'answer': false},
-    {'question': 'Dart is used to build Flutter apps.', 'answer': true},
-    {'question': 'Flutter is only for Android development.', 'answer': false},
+  final List<Widget> _pages = [
+    HomePage(),
+    ProfilePage(),
   ];
-
-  void _answerQuestion(bool userAnswer) {
-    setState(() {
-      if (_questionIndex < _questions.length - 1) {
-        _questionIndex++;
-      } else {
-        _showCompletionDialog();
-      }
-    });
-  }
-
-  void _showCompletionDialog() {
-    showDialog(
-      context: context,
-      builder: (ctx) => AlertDialog(
-        title: Text("Quiz Completed!"),
-        content: Text("You have answered all the questions."),
-        actions: [
-          TextButton(
-            onPressed: () {
-              Navigator.of(ctx).pop();
-              setState(() {
-                _questionIndex = 0; // Restart Quiz
-              });
-            },
-            child: Text("Restart"),
-          )
-        ],
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter Quiz'),
-        centerTitle: true,
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) => setState(() => _selectedIndex = index),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("University Portal")),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            DrawerHeader(child: Text('Menu', style: TextStyle(fontSize: 24))),
+            ListTile(title: Text('Dashboard')),
+            ListTile(title: Text('Settings')),
+            ListTile(title: Text('Logout')),
+          ],
+        ),
       ),
       body: Center(
-        child: _questionIndex < _questions.length
-            ? Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              _questions[_questionIndex]['question'] as String,
-              style: TextStyle(fontSize: 22),
-              textAlign: TextAlign.center,
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (_) => DetailPage()));
+          },
+          child: Hero(
+            tag: 'course',
+            child: Card(
+              elevation: 4,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Image.network('https://foundr.com/wp-content/uploads/2021/09/Best-online-course-platforms.png', width: 300),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("Flutter Development", style: TextStyle(fontSize: 18)),
+                  ),
+                ],
+              ),
             ),
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () => _answerQuestion(true),
-              child: Text('True'),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => _answerQuestion(false),
-              child: Text('False'),
-            ),
-          ],
-        )
-            : Text(
-          "Quiz Completed!",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
   }
 }
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Course Details")),
+      body: Center(
+        child: Hero(
+          tag: 'course',
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.network('https://www.classcentral.com/report/wp-content/uploads/2022/09/BCG-Flutter-Featured-Banner-768x384.png', width: 300),
+              SizedBox(height: 16),
+              Text("Flutter Development", style: TextStyle(fontSize: 24)),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text("Welcome to the Flutter course!"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile")),
+      body: Center(child: Text("User Profile Info")),
+    );
+  }
+}
+  
